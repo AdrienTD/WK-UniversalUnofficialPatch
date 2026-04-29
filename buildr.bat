e64f5b8310b34941aaf8fea52fc01e74c7c50e41
AdrienTD <agpetit69@gmail.com>
2026-04-29 19:21:26 +0200

    Rename *.c to *.cpp

--- buildr.bat
+++ buildr.bat
@@ -1,4 +1,4 @@
 @set PATCHVER=0,92,0,0
 @rc /dPATCHVER=%PATCHVER% resource.rc
-@cl %* /c /DPATCHVER=%PATCHVER% /LD *.c
+@cl %* /c /DPATCHVER=%PATCHVER% /LD *.cpp
 @link /DLL /DEF:d3d8.def /OUT:d3d8.dll *.obj resource.res user32.lib version.lib ole32.lib strmiids.lib
